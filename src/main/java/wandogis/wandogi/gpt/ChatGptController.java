e22dbbe571e11e3a1ff1c7cfc40a1fa0d029f55d
SooyaDolphin <120730055+SooyaDolphin@users.noreply.github.com>
2023-05-26 15:56:08 +0900

    [Feat]챗지피티 기본세팅

---
+++ src/main/java/wandogis/wandogi/gpt/ChatGptController.java
@@ -0,0 +1,21 @@
+package wandogis.wandogi.gpt;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chat-gpt")
+public class ChatGptController {
+    private final ChatGptService chatGptService;
+
+    public ChatGptController(ChatGptService chatGptService) {
+        this.chatGptService = chatGptService;
+    }
+
+    @PostMapping("/question")
+    public ChatGptResponseDto sendQuestion(@RequestBody QuestionRequestDto requestDto) {
+        return chatGptService.askQuestion(requestDto);
+    }
+}

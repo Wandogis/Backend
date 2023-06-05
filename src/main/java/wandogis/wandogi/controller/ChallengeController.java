e269f7c33ec98ec76c054deb3979ea9892463996
JongeunKeum <jon9eun@gmail.com>
2023-06-05 15:30:31 +0900

    [Fix] 챌린지 목록 API 수정 - RestAPI로 보내게

--- src/main/java/wandogis/wandogi/controller/ChallengeController.java
+++ src/main/java/wandogis/wandogi/controller/ChallengeController.java
@@ -1,16 +1,15 @@
 package wandogis.wandogi.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import wandogis.wandogi.domain.Challenges;
 import wandogis.wandogi.service.ChallengeService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("challenges")
 public class ChallengeController {
@@ -20,43 +19,39 @@ public class ChallengeController {
      * 진행 예정인 챌린지 목록 인기순
      */
     @GetMapping("/list/expected-view")
-    public String expectedViewList(Model model) {
+    public Object expectedViewList() {
         List<Challenges> expectedList = challengeService.getChallengeListsByStartDate();
         List<Challenges> viewList = challengeService.getChallengeListsByView(expectedList);
-        model.addAttribute("viewList", viewList);
-        return "challenges/expectedViewList";
+        return viewList;
     }
 
     /**
      * 진행 예정인 챌린지 목록 최신순
      */
     @GetMapping("/list/expected-latest")
-    public String expectedLatestList(Model model) {
+    public Object expectedLatestList() {
         List<Challenges> expectedList = challengeService.getChallengeListsByStartDate();
         List<Challenges> latestList = challengeService.getChallengeListsByDate(expectedList);
-        model.addAttribute("latestList", latestList);
-        return "challenges/expectedLatestList";
+        return latestList;
     }
 
     /**
      * 진행 중인 챌린지 인기순
      */
     @GetMapping("/list/going-view")
-    public String goingViewList(Model model) {
+    public Object goingViewList() {
         List<Challenges> goingList = challengeService.getChallengeListsByEndDateAndStartDate();
         List<Challenges> viewList = challengeService.getChallengeListsByView(goingList);
-        model.addAttribute("viewList", viewList);
-        return "challenges/goingViewList";
+        return viewList;
     }
 
     /**
      * 진행 중인 챌린지 최신순
      */
     @GetMapping("/list/going-latest")
-    public String goingLatestList(Model model) {
+    public Object goingLatestList() {
         List<Challenges> goingList = challengeService.getChallengeListsByEndDateAndStartDate();
         List<Challenges> latestList = challengeService.getChallengeListsByDate(goingList);
-        model.addAttribute("latestList", latestList);
-        return "challenges/goingLatestList";
+        return latestList;
     }
 }

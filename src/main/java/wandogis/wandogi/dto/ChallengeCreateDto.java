47d3bd90b13d3d2d1d68fd4603f70fb6c6c85e7f
JongeunKeum <jon9eun@gmail.com>
2023-06-09 15:13:19 +0900

    [Feat] 챌린지 생성 - 참여 유저와 진도율은 아직 저장 안 됨

---
+++ src/main/java/wandogis/wandogi/dto/ChallengeCreateDto.java
@@ -0,0 +1,38 @@
+package wandogis.wandogi.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import wandogis.wandogi.domain.Challenges;
+import wandogis.wandogi.domain.Users;
+
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.Date;
+
+@Getter
+@Setter
+public class ChallengeCreateDto {
+    @Id
+    private ObjectId id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String photo;
+    private String description;
+    private String category;
+    private int page;
+    private int view;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date startDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date endDate;
+
+    public Challenges toEntity() {
+        return Challenges.builder().id(id).title(title).author(author).photo(photo)
+                .description(description).category(category).page(page).view(view)
+                .startDate(startDate).endDate(endDate).build();
+    }
+}

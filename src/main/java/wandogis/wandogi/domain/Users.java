c78d7de00a475b4bac894b870bce645abd8b6b82
JongeunKeum <jon9eun@gmail.com>
2023-08-16 10:29:26 +0900

    [Fix] Users domain 수정 - field 추가 및 변경

--- src/main/java/wandogis/wandogi/domain/Users.java
+++ src/main/java/wandogis/wandogi/domain/Users.java
@@ -1,20 +1,31 @@
 package wandogis.wandogi.domain;//테이블정보
 
 import lombok.Data;
+import org.bson.types.ObjectId;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Data
+@Entity
 public class Users {
-    private String loginid;
-    private String password;
-    private String name;
-    private String photo;
-    private String genre;
-    private String age;
-    private String challenge;
-
-    @Override
-    public String toString() {
-        return "name is " + name;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ObjectId id;        // DB 자동 생성
+    @Column(nullable = false)
+    private String email;       // 가입되어있는 회원인지 확인함
+    @Column(nullable = false)
+    private String nickname;    // 사용자 닉네임
+    @Column(nullable = false)
+    private int socialType;     // 1: Naver, 2: Kakao
+    @Column(nullable = false)
+    private String photo;       // 사용자 프로필 이미지
+    private String genre;       // 도서 추천 때 선택 입력(선호하는 장르)
+    private int age;            // 도서 추천 때 선택 입력
+    private String gender;      // 도서 추천 때 선택 입력
+    private int ranking;        // 완료한 챌린지 개수에 따른 오늘의 순위
+    private int numOfChallenge; // 완료한 챌린지 개수
+    private List<ObjectId> books;      // 유저가 선호하는 책(Books와 One-to-Many)
+    private List<ObjectId> challenges; // 유저가 진행한 챌린지(Challenges와 Many-to-Many)
+    private List<ObjectId> calendars;  // 유저 달력(날짜)(Calendars와 One-to-Many)
 }
-
